b3fe38dc31dba6d29dafcbeffdeb8a9d481fb883
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-01-16 10:43:05 +0300

    done with assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,15 @@ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p> I made this</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href="https://github.com/liza">Github link</a>
+      <a href="https://www.linkedin.com/in/liza/">Linkedin link</a>
+
     </div>
-  );
+  ); 
 }
 
 export default About;
